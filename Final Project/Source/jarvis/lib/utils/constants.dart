cf8833a6b805f791011ee1baea9b440ef6aeb25b
ThinhTranNT <trangiathinh18092003@gmail.com>
2024-12-17 16:03:21 +0700

    get kburl

--- Final Project/Source/jarvis/lib/utils/constants.dart
+++ Final Project/Source/jarvis/lib/utils/constants.dart
@@ -2,6 +2,7 @@ import 'package:jarvis/models/assistant.dart';
 
 class ProjectConstants {
   static const String baseUrl = 'https://api.dev.jarvis.cx';
+  static const String kbUrl = 'https://knowledge-api.dev.jarvis.cx/';
 
   static List<Assistant> defaultAssistants = [
     Assistant(
